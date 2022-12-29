216af3b5f807fa478e4532b1b1a7a80b9fc6f9e0
kumkum <kumkumrathour00@gmail.com>
2022-12-29 15:38:41 +0530

    add

---
+++ leedcodeGithub/src/leedcode/lastWordLength.java
@@ -0,0 +1,22 @@
+package leedcode;
+
+public class lastWordLength {
+    public static void main(String[] args){
+        System.out.println("hello");
+        String str = " hello World ";
+        System.out.println(LengthOfLastString(str));
+    }
+    static int LengthOfLastString (String s){
+        String str = s.trim();
+        int count = 0;
+        for(int i = str.length()-1 ; i>=0 ; i--){
+          if(str.charAt(i) != ' '){
+            count++;
+          }
+          else{
+            break;
+          }
+        }
+        return count;
+    }
+}

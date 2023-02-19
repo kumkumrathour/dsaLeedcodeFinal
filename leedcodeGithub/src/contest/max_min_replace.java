83c7bf07f83397788475baaa9462b3dbb4f07f8f
kumkumrathour <kumkumrathour00@gmail.com>
2023-02-19 21:44:25 +0530

    add

--- leedcodeGithub/src/contest/max_min_replace.java
+++ leedcodeGithub/src/contest/max_min_replace.java
@@ -4,6 +4,8 @@ public class max_min_replace {
     public static void main(String[] args){
         String nums= "8764";
         // int digit = charValue(nums);
+         String name = "kumkum";
+System.out.println("hello"+ name);
     }
     // public static int charValue(String nums){
         // String num = ParseInt(nums/10);

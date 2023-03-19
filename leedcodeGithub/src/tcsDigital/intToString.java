c5f1e67699dcba2e991e0500c58155e557469677
kumkumrathour <neharathour9669535194@gmail.com>
2023-03-19 13:46:20 +0530

    add

---
+++ leedcodeGithub/src/tcsDigital/intToString.java
@@ -0,0 +1,11 @@
+package tcsDigital;
+import java.util.*;
+public class intToString {
+    public static void main(String[] args){
+        System.out.println("enter");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s= Integer.toString(n);
+        System.out.println(s);
+    }
+}
